24adb14d982711ac6ae7f0dd55cad0b7055e9f4f
Sean McNeeley <sean21mcn@gmail.com>
2017-11-15 15:21:29 -0500

    initial commit with basic folder structure, made with create-react-app

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'; //change
+import registerServiceWorker from './registerServiceWorker';
+
+
+// import all(?) components
+// define Game component, which is called in ReactDOM.render
+
+
+
+
+ReactDOM.render(<Game />, document.getElementById('root'));
+registerServiceWorker();
