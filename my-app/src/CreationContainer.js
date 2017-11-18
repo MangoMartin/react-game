b9d39fc1c38aa90b99670d9f2dcaffa2725e3596
mistermappy123 <mikhal.gelo@gmail.com>
2017-11-18 18:01:52 -0500

    character creation page

--- my-app/src/CreationContainer.js
+++ my-app/src/CreationContainer.js
@@ -1,1 +1,58 @@
 // for character creation
+import React, { Component } from 'react' ;
+
+class CreateCharacter extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			isHidden : true
+		};
+
+		this.blinky = this.blinky.bind(this)
+	};
+
+	render(){
+		return(
+			<div 
+			id = 'CreateCharacter'>
+			<h1>Create Character</h1>
+			 
+			<label>Character Name :</label><input
+			type = 'text' /><br/>
+			<label>Race :</label> 
+				<select name="Race">
+    				<option value="Human">Human</option>
+    				<option value="Undead">Undead</option>
+    				<option value="Elf">Elf</option>
+    				<option value="Orc">Orc</option>
+  				</select><br/>
+			<label>Gender :</label> 
+				<select name="Gender">
+					<option value="Male">Male</option>
+					<option value="Female">Female</option>
+				</select>
+			<br/><br/>
+			
+			<div
+				id  = 'blinker'
+				className = {this.state.isHidden ? 'default-mode' : 'non-default-mode'}
+				onLoad={this.blinky()}>
+				PRESS START
+			</div>
+			</div>
+
+			)
+	}
+
+	blinky() {
+		setTimeout(() => {
+		let currentState = this.state.isHidden
+		//console.log(this.state.isHidden)
+		this.setState({isHidden : !currentState})	
+	}, 500)}
+	
+}
+
+export default CreateCharacter ;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; //change
+import CreateCharacter from './CreationContainer.js'
 import registerServiceWorker from './registerServiceWorker';
 
 
@@ -11,5 +11,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<CreateCharacter />, document.getElementById('root'));
 registerServiceWorker();
