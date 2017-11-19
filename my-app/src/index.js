db569685897483bc6b4f45e1027af6f3338bc9fc
Sean McNeeley <sean21mcn@gmail.com>
2017-11-19 11:45:34 -0500

    created rudimentary outline of Game component in index.js, commented

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,6 +9,20 @@ import registerServiceWorker from './registerServiceWorker';
 // import all(?) components
 // define Game component, which is called in ReactDOM.render
 
+// return all containers in a div; in game, bind state, all defined funcs and scenes
+// class Game extends Component {
+//   constructor(props) {
+//
+//   }
+//
+//   render() {
+//     let game = {
+//       state: this.state,
+//       setState: this.setState.bind(this)
+//     }
+//     return null;
+//   }
+// }
 
 
 
