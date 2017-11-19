14b35b2d562600f4c3320b993f97186b63c8e710
Sean McNeeley <sean21mcn@gmail.com>
2017-11-19 11:39:41 -0500

    added ValueInput and OptionSelect to GameMisc

--- my-app/src/GameMisc.js
+++ my-app/src/GameMisc.js
@@ -86,6 +86,78 @@ export class GameText extends Component {
   }
 }
 
+// formerly GameInput; check the line break after label
+export class ValueInput extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value || ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({
+              value: event.target.value
+            });
+
+            if(typeof(this.props.onChange) === 'function'){
+              this.props.onChange(event, event.target.value, this.props.onChangeArgs);
+            }
+          }}
+        />
+        <label>{this.props.text}</label><br />
+      </div>
+    );
+  }
+
+}
+
+// formerly GameSelect; check the line break after label
+export class OptionSelect extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value || this.props.options[0]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <select value={this.state.value} onChange={(event) => {
+          this.setState({
+              value: event.target.value
+            });
+
+            if(typeof(this.props.onChange) === 'function') {
+              this.props.onChange(event, event.target.value, this.props.onChangeArgs);
+            }
+          }}>
+
+          {this.props.options.map((option, index) => {
+            return <option
+              key={'option-' + index}>
+                {option}
+              </option>;
+            })}
+        </select>
+
+        <label>{this.props.text}</label><br />
+      </div>
+    );
+  }
+}
+
 export class GameImage extends Component {
   render() {
     return (
@@ -93,6 +165,55 @@ export class GameImage extends Component {
         src={this.props.src}
         alt=''
       />
+    );
+  }
+}
+
+//from style: display: 'inline', marginLeft: '5%'
+
+export class ItemMenu extends Component {
+  render() {
+    return (
+      <div class Name='item-menu'>
+        <h3>
+        {this.props.item.getName(this.props.game)}<DropdownMenu
+          style={{
+
+          }}
+          options={[
+            {
+              text: 'Discard',
+              onClick: () => {
+                if(confirm('Do you really want to throw out your ' + this.props.item.name + '?')){
+                  this.props.game.takePlayerItem(this.props.item);
+                }
+              },
+            },
+            {
+              // update this to perform an action instead of alert
+              text: 'Use',
+              onClick: () => alert('Use clicked!'),
+              show: this.props.item.type.includes('usable-item')
+            },
+            {
+              text:'Equip',
+              onClick: () => {
+                this.props.game.equipItem(this.props.item.id, this.props.game);
+              },
+              show: !this.props.item.equipped && !!this.props.item.equipType
+            },
+            {
+              text: 'Un-Equip',
+              onClick: () => {
+                this.props.game.unEquipItem(this.props.item.id, this.props.game);
+              },
+              show: !!this.props.item.equipped && !!this.props.item.equipType
+            }
+          ]}
+        />
+        </h3>
+        <p>{this.props.item.getDescription(this.props.game)}</p>
+      </div>
     );
   }
 }
