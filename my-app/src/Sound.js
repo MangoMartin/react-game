cf1bcc6184091dfea53ea5c82df4845ee32fe116
Sean McNeeley <sean21mcn@gmail.com>
2017-12-01 01:23:38 -0500

    added comment crediting source in Sound.js

--- my-app/src/Sound.js
+++ my-app/src/Sound.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import sound from './audio/LabyrinthCut.mp3';
 //check for react-sound, react-audio-player, sound manager 2
 
-// maybe add control function in settings? that, or a toggle audio button
+// Song: "Labyrinth" from The Legend of Zelda (NES), composed by Koji Kondo
+
+// consider adding control function in settings? that, or a toggle audio button
 class BackgroundAudio extends Component {
 
   render() {
